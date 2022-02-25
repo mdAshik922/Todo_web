f6b2c6497e484dde6af68af74ca859304531a149
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-25 18:38:37 +0600

    commit

--- Todo.js
+++ Todo.js
@@ -4,3 +4,14 @@ function getById(id){
 
 const add_btn  = getById('addNewBtn');
 const newTaskInp = getById('newTaskInp');
+
+add_btn.addEventListener('click', function(e){
+    let taskName = newTaskInp.value;
+    if(!taskName){
+        alert("please insert a new task");
+    }
+    else{
+        newTaskInp.value = '';
+    }
+    console.log(taskName)
+})
