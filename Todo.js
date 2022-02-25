299fc9b758cd639ac944714fac9471f79be1a2c2
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-25 20:13:15 +0600

    commit

--- Todo.js
+++ Todo.js
@@ -1,9 +1,10 @@
 function getById(id){
-    return document.getElementById(id)
+    return document.getElementById(id);
 };
 
 const add_btn  = getById('addNewBtn');
 const newTaskInp = getById('newTaskInp');
+const taskList = getById('task_list');
 
 add_btn.addEventListener('click', function(e){
     let taskName = newTaskInp.value;
@@ -12,20 +13,15 @@ add_btn.addEventListener('click', function(e){
         return ;
     }
    
-    
-            newTaskInp.value = '';
-        
-    
-        addNewItem(taskName)
+            newTaskInp.value = '';  
+        addNewItem(taskName);
     // console.log(taskName)
-})
+});
 
 function addNewItem(text){
     const item = document.createElement('div');
     item.className = 'item';
-    item.innerHTML = ` <li>${text}</li>
-    <button class="edit"><span class="material-icons-outlined">edit</span></button>
-    <button class="complete"><span class="material-icons-outlined">check</span></button>
-    <button class="delete"><span class="material-icons-outlined"> delete</span></button>`
-console.log(text);
-}
+    item.innerHTML = "<h2>hello</h2>";
+    taskList.appendChild(item);
+// console.log(text);
+};
